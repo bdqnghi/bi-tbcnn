fe38d61f900017dd428c57bcc5d0d6befa3137ee
Quoc Nghi <bdqnghi@gmail.com>
2017-10-06 22:33:09 +0800

    update test pickle

--- ast-node-encoding/scripts/test_pickle.py
+++ ast-node-encoding/scripts/test_pickle.py
@@ -1,7 +1,7 @@
 import pickle
 
-with open("./data/algorithms.pkl", 'rb') as f:
+with open("./data/algorithm_trees.pkl", 'rb') as f:
     nodes = pickle.load(f)
 
 # print len(vectors[0])
-print len(nodes)
+print nodes[5]
