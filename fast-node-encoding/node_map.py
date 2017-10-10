6d91cf665181733a18f10838c92254163526b035
Quoc Nghi <bdqnghi@gmail.com>
2017-10-10 21:35:44 +0800

    step to create training trees

--- fast-node-encoding/node_map.py
+++ fast-node-encoding/node_map.py
@@ -6,4 +6,6 @@ with open("./data/fast_node_map.tsv","r") as f:
 for line in lines:
     splits = line.split("=")
     # FAST_NODE_MAP[splits[0].strip()] = int(splits[1].strip())
-    FAST_NODE_MAP[int(splits[1].strip())] = splits[0].strip()
+    FAST_NODE_MAP[str(splits[1].strip())] = splits[0].strip()
+
+# print FAST_NODE_MAP[0]
