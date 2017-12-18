8a17651514b06425f9c256db71987a9881d57800
Yijun Yu <y.yu@open.ac.uk>
2017-12-18 17:15:09 +0000

    the missing script

---
+++ bi-tbcnn/bi-tbcnn/prepare_pairs_data.py
@@ -0,0 +1,36 @@
+import pickle
+import sampling
+import random
+import sys
+
+# "./data/cpp_algorithms_trees.pkl"
+with open(sys.argv[0], 'rb') as fh:
+ 	# left_trees, _, left_algo_labels = pickle.load(fh)
+ 	_, left_trees, left_algo_labels = pickle.load(fh)
+
+#"./data/java_algorithms_trees.pkl"
+with open(sys.argv[1], 'rb') as fh:
+	# right_trees, _, right_algo_labels = pickle.load(fh)
+	_, right_trees, right_algo_labels = pickle.load(fh)
+
+all_1_pairs, all_0_pairs = sampling.get_all_pairs_for_training(left_trees,right_trees)
+print "1 pairs:" + str(len(all_1_pairs)) + " 0 pairs:" + str(len(all_0_pairs))
+
+random.shuffle(all_1_pairs)
+random.shuffle(all_0_pairs)
+
+
+random_1_pairs = random.sample(all_1_pairs,2000)
+random_0_pairs = random.sample(all_0_pairs,2000)
+
+
+
+all_testing_random_pairs = random_1_pairs + random_0_pairs
+random.shuffle(all_testing_random_pairs)
+#"./data/4000_testing_pairs.pkl"
+
+with open(sys.argv[2], 'wb') as file_handler:
+	pickle.dump(all_testing_random_pairs, file_handler)
+	file_handler.close()
+
+
